4872f590e3955acdfefb692eaf4e8baceb564574
llpvqll <llpvo2602ll@gmail.com>
2021-08-17 13:49:42 +0300

    upadate second task

--- exeptionLessons/second_task.py
+++ exeptionLessons/second_task.py
@@ -1,52 +1,50 @@
 
-class Calculator:
-    def __init__(self):
-        while True:
-            print("Welcome to Calculator!")
-            try:
-                first_num = int(input("Enter first num: "))
-                action = input("Choose action (+, -, *, /, **): ")
-                second_num = int(input("Enter second num: "))
 
-                def condition(a):
-                    if a == "+":
-                        return addition()
-                    elif a == "-":
-                        return subtraction()
-                    elif a == "*":
-                        return multiplication()
-                    elif a == "/":
-                        return division()
-                    elif a == "**":
-                        return power()
+def calculator():
+    print("Welcome to Calculator!")
+    while True:
+        try:
+            first_num = int(input("Enter first num: "))
+            action = input("Choose action (+, -, *, /, **): ")
+            second_num = int(input("Enter second num: "))
 
-                def addition():
-                    result = first_num + second_num
-                    print(f"{first_num} {action} {second_num} = {result}")
+            def condition(a):
+                if a == "+":
+                    return addition()
+                elif a == "-":
+                    return subtraction()
+                elif a == "*":
+                    return multiplication()
+                elif a == "/":
+                    return division()
+                elif a == "**":
+                    return power()
 
-                def subtraction():
-                    result = first_num - second_num
-                    print(f"{first_num} {action} {second_num} = {result}")
+            def addition():
+                result = first_num + second_num
+                print(f"{first_num} {action} {second_num} = {result}")
 
-                def multiplication():
-                    result = first_num * second_num
-                    print(f"{first_num} {action} {second_num} = {result}")
+            def subtraction():
+                result = first_num - second_num
+                print(f"{first_num} {action} {second_num} = {result}")
 
-                def division():
-                    result = first_num / second_num
-                    print(f"{first_num} {action} {second_num} = {result}")
+            def multiplication():
+                result = first_num * second_num
+                print(f"{first_num} {action} {second_num} = {result}")
 
-                def power():
-                    result = first_num ** second_num
-                    print(f"{first_num} {action} {second_num} = {result}")
+            def division():
+                result = first_num / second_num
+                print(f"{first_num} {action} {second_num} = {result}")
 
-                condition(action)
+            def power():
+                result = first_num ** second_num
+                print(f"{first_num} {action} {second_num} = {result}")
 
-            except (ZeroDivisionError, ValueError, UnboundLocalError):
-                print(f'You enter incorrect values, try again!')
-                return Calculator()
+            condition(action)
+
+        except (ZeroDivisionError, ValueError):
+            print(f'You enter incorrect values, try again!')
+            return calculator()
 
 
-calc = Calculator()
-print(calc)
-
+calculator()

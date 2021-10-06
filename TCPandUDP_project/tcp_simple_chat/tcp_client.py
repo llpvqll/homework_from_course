f259e5e8e7178d7d97c9d53d62ce359489c35f69
llpvqll <llpvo2602ll@gmail.com>
2021-10-06 16:47:34 +0300

    update tcp server task

---
+++ TCPandUDP_project/tcp_simple_chat/tcp_client.py
@@ -0,0 +1,9 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(('127.0.0.1', 8888))
+
+sock.send(b'Hi, you create TCP server ;)')
+result = sock.recv(64)
+print('Response:', result)
+sock.close()

d54621ed89b6d730e622454dda737d2d438fb9be
llpvqll <llpvo2602ll@gmail.com>
2021-10-12 12:14:33 +0300

    made second addition task

--- data_base/materials_table.py
+++ data_base/materials_table.py
@@ -1,6 +1,25 @@
 import csv
 
-headers = ['ID', 'weight', 'height', 'add_specification']
+quoting = csv.QUOTE_ALL
+
+headers = ['ID', 'weight', 'height', 'add specification']
 with open('materials.csv', 'w', encoding="UTF-8") as file:
-    writer = csv.writer(file)
-    writer.writeheader
+    # create headers in file
+    writer = csv.DictWriter(file, fieldnames=headers, quoting=quoting)
+    writer.writeheader()
+    # add users
+    # add first user
+    writer.writerow({
+        'ID': 1,
+        'weight': 90,
+        'height': 181,
+        'add specification': ['Vladislav', 'administrator']
+    })
+    # add second user
+    writer.writerow({
+        'ID': 2,
+        'weight': 70,
+        'height': 181,
+        'add specification': ['Vlad', 'deputy administrator']
+    })
+

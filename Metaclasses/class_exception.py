0735aa559af622e0d847e78929665d026ce7b744
llpvqll <llpvo2602ll@gmail.com>
2021-10-20 13:21:18 +0300

    made third task

--- Metaclasses/class_exception.py
+++ Metaclasses/class_exception.py
@@ -1,2 +1,32 @@
 class Except(type):
-    pass
+
+    def __new__(mcs, class_name, base_class, attrs):
+        new_attrs = {}
+
+        for attr, value in attrs.items():
+            for item in attr:
+                if item != item.lower():
+                    raise NameError('Invalid character in the attribute/method name!')
+
+                try:
+                    int(item)
+                    raise NameError('Invalid character in the attribute/method name!')
+                except ValueError:
+
+                    continue
+            new_attrs[attr.lower()] = value
+        return type(class_name, base_class, new_attrs)
+
+
+attributes = {
+    'speed': 120,
+    'mark': 'Audi',
+    'color': 'Green'}
+
+
+Car = Except.__new__(Except, 'Car', (), attributes)
+
+print(Car)
+print(type(Car))
+print(Car.__class__)
+print(Car.color)

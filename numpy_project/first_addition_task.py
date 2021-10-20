6fbbce94fe828cae729b763119b8db2b877cd07e
llpvqll <llpvo2602ll@gmail.com>
2021-10-20 16:19:06 +0300

    made first addition task

--- numpy_project/first_addition_task.py
+++ numpy_project/first_addition_task.py
@@ -1,2 +1,11 @@
-import numpy
+import numpy as np
+
+print('=' * 100)
+a = np.arange(625).reshape(25, 25)
+print(a)
+print('=' * 100)
+b = np.diag([i for i in range(25)])
+print(b)
+print('=' * 100)
+print(a * b)
 

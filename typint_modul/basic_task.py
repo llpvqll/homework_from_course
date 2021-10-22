3c457ece31f4274c3b21a436ccbd1e78da91b522
llpvqll <llpvo2602ll@gmail.com>
2021-10-22 17:16:43 +0300

    update last task

--- typint_modul/basic_task.py
+++ typint_modul/basic_task.py
@@ -4,13 +4,14 @@ def value_changer(int_list: list[int]) -> list[str]:
     for item in int_list:
         item = str(item)
         str_list.append(item)
-    print(str_list)
+    return str_list
 
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(f"List before function: \n{lst}")
 print()
-print(f"List after function: ")
-value_changer(lst)
+result = value_changer(lst)
+print(f"List after function: \n{result}")
+
 
 
